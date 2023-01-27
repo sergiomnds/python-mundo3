fb5acf17621be5281b93c30ae9300bfd38949bb5
sergio mendes <srgmendesl@gmail.com>
2023-01-27 16:45:11 -0300

    aula e ex

--- exerciciosPython/ex076.py
+++ exerciciosPython/ex076.py
@@ -1,17 +1,17 @@
 
-#? Uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
-#? No final, mostre uma listagem de preços, organizando os dados em forma tabular.
+# ? Uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
+# ? No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 
 produtos = ('Bolacha Maria', 9.90,
-    'Miojo', 4.50,
-    'Salsicha', 22.35,
-    'Arroz', 7.80,
-    'Feijão', 9.20,
-    'Cream Cracker', 1.50)
+            'Miojo', 4.50,
+            'Salsicha', 22.35,
+            'Arroz', 7.80,
+            'Feijão', 9.20,
+            'Cream Cracker', 1.50)
 print('-'*40)
 print(f'{"LISTAGEM DE PREÇOS":^40}')
 print('-'*40)
-for index in range(0, len(produtos)):
+for index in enumerate(produtos):
     if index % 2 == 0:
         print(f'{produtos[index]:.<30}', end='')
     elif index % 2 == 1:
--- listasPython/aula18.py
+++ listasPython/aula18.py
@@ -1,11 +1,12 @@
-
-#* Continuando a última aula, agora trabalhando listas dentro de outras listas.
+'''
+    Continuando a última aula, agora trabalhando listas dentro de outras listas.
+'''
 teste = list()
 teste.append('Gustavo')
 teste.append(40)
 
 galera = list()
-galera.append(teste[:])
+galera.append(teste[:]) #? Uso dos [:] para dizer que é uma cópia e não uma ligação
 teste[0] = 'Maria'
 teste[1] = 22
 galera.append(teste[:])
