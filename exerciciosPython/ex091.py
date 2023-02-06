e0f357f2d709205469d0c8497a5880c7900618b9
sergio mendes <srgmendesl@gmail.com>
2023-02-06 13:54:09 -0300

    exercicio 91 feito

--- exerciciosPython/ex091.py
+++ exerciciosPython/ex091.py
@@ -3,8 +3,21 @@ Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios
 Guarde esses resultados em um dicionário em Python.
 No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 '''
-
+from operator import itemgetter
 from random import randint
-
-sorte = {}
-# for c in (0, 4):
+from time import sleep
+sorte = {
+        'Jogador 01': randint(1, 6),
+        'Jogador 02': randint(1, 6),
+        'Jogador 03': randint(1, 6),
+        'Jogador 04': randint(1, 6)
+        }
+print('Os números sorteados foram: ')
+for k, v in sorte.items():
+    print(f'{k} tirou {v} no dado')
+    sleep(1)
+    
+ranking = sorted(sorte.items(), key=itemgetter(1), reverse=True)
+for i, v in enumerate(ranking):
+    print(f'{i+1}º Lugar: {v[0]} com {v[1]}')
+    sleep(1)
