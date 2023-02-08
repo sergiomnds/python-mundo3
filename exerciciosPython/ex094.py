f79d2733746487344f403ebbc68ac35811f09530
sergio mendes <srgmendesl@gmail.com>
2023-02-08 15:09:18 -0300

    94 fazendo

--- exerciciosPython/ex094.py
+++ exerciciosPython/ex094.py
@@ -9,13 +9,18 @@ D) Uma lista de pessoas com idade acima da média
 listaPessoas = []
 pessoa = {}
 totalIdade = 0
+mulheres = []
+listaAcimaMedia = []
 while True:
     pessoa['Nome'] = str(input('Nome da Pessoa: '))
-    pessoa['Sexo'] = str(input('Sexo (M ou F): '))
+    pessoa['Sexo'] = str(input('Sexo (M ou F): ')).upper()
     pessoa['Idade'] = int(input(f'Idade de {pessoa["Nome"]}: '))
     totalIdade += pessoa['Idade']
     listaPessoas.append(pessoa.copy())
     
+    if pessoa['Sexo'] == 'F':
+        mulheres.append(pessoa['Nome'])
+
     resposta = str(input('Deseja continuar? [S/N]: '))
     if resposta in 'Nn':
         break
@@ -24,3 +29,9 @@ media = totalIdade / len(listaPessoas)
    
 print(f'Qnt. de pessoas cadastradas: {len(listaPessoas)}')
 print(f'Média de Idade: {media}')
+print('As mulheres são: ',end='')
+for k, m in enumerate(mulheres):
+    if k == len(mulheres):
+        print(m)
+    else:
+        print(m, end='')
