ad9e92518b0423f068b275e0388e780533e0d7ab
Sergio Mendes <srgmendesl@gmail.com>
2023-02-09 23:31:06 -0300

    95 feito

--- exerciciosPython/ex095.py
+++ exerciciosPython/ex095.py
@@ -3,24 +3,45 @@ Aprimore o desafio 93 para que ele funcione com vários jogadores,
 incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
 '''
 
-jogador = {}
-jogador['Nome do Jogador'] = str(input('Nome do Jogador: ')).upper()
-jogador['Nº de Partidas'] = int(input(f'Qnt. de Partidas de {jogador["Nome do Jogador"]}: '))
+listaJogadores = []
+while True:
+    jogador = {}
+    jogador['Nome do Jogador'] = str(input('Nome do Jogador: ')).upper()
+    jogador['Nº de Partidas'] = int(input(f'Qnt. de Partidas de {jogador["Nome do Jogador"]}: '))
 
-gols = []
-totalGols = 0
-for c in range(0, jogador['Nº de Partidas']):
-    gol = int(input(f'Quantos gols foi feito na {c+1}º partida ? '))
-    gols.append(gol)
-jogador['Gols por Partida'] = gols
-jogador['Total de Gols feitos no Campeonato'] = sum(gols)
+    gols = []
+    totalGols = 0
+    for c in range(0, jogador['Nº de Partidas']):
+        gol = int(input(f'Quantos gols foi feito na {c+1}º partida ? '))
+        gols.append(gol)
+    jogador['Gols por Partida'] = gols
+    jogador['Total de Gols feitos no Campeonato'] = sum(gols)
+    
+    listaJogadores.append(jogador.copy())
+    resposta = str(input('Deseja continuar? [S/N]')).upper()[0]
+    if resposta == 'N':
+        break
 
 print('='*30)
-for k, v in jogador.items():
-    print(f'{k}: {v}')
+print('Código - Nome - Gols - Total')
+print('-'*20)
+for j in listaJogadores:
+    print(listaJogadores.index(j), end=' ')
+    for v in j.values():
+        if v == j['Total de Gols feitos no Campeonato']:
+            print(v)
+        else:
+            print(v, end= ' ')
+print('-'*20)
 
-print('='*30)
-print(f'O jogador {jogador["Nome do Jogador"]} jogou {jogador["Nº de Partidas"]} partidas')
-for c, v in enumerate(gols):
-    print(f'Na {c+1}º partida fez {v} gols')
-print(f'No total foram {jogador["Total de Gols feitos no Campeonato"]} gols no campeonato')
+codigo = 0
+while True:
+    codigo = int(input('Mostrar dados de qual jogador? (999 para PARAR)'))
+    if codigo == 999:
+        print('FINALIZADO')
+        break
+    else:
+        print(f'LEVANTAMENTO DO JOGADOR {listaJogadores[codigo]["Nome do Jogador"]}')
+        for c, v in enumerate(listaJogadores[codigo]["Gols por Partida"]):
+            print(f'Na {c+1}º partida fez {v} gols')
+        print('-'*20)
