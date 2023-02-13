31c143896a2ba1a19084e1188939241815bc0671
sergio mendes <srgmendesl@gmail.com>
2023-02-13 14:41:39 -0300

    ex101

--- exerciciosPython/ex101.py
+++ exerciciosPython/ex101.py
@@ -4,3 +4,11 @@ que vai receber como parâmetro o ano de nascimento de uma pessoa,
 retornando um valor literal indicandose uma pessoa
 tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 '''
+from datetime import datetime
+
+def voto(ano):
+    idade = datetime.now().year - ano
+    if idade < 18:
+        return 'NEGADO'
+    # elif 18 <= idade < 70:
+        
