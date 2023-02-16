45be5b27023bfe2c33396e4fd1a6e3cd071b9ca5
sergio mendes <srgmendesl@gmail.com>
2023-02-16 14:38:55 -0300

    ex112 feito

--- exerciciosPython/utilidadesCeV/dado/__init__.py
+++ exerciciosPython/utilidadesCeV/dado/__init__.py
@@ -1,4 +1,34 @@
 def leiaDinheiro(texto):
-    preco = str(input(texto))
-    if ',' in preco:
-        return preco.replace(',','.')
+    preco = str(input(texto)).strip()
+    while True:
+        if preco.isalpha():
+            print(f'\033[0;31mERR: "{preco}" não aceito!\033[m')
+            preco = str(input(texto))
+
+        if preco == '':
+            print(f'\033[0;31mERR: "{preco}" não aceito!\033[m')
+            preco = str(input(texto))
+
+        if preco.isnumeric() is True:
+            break
+
+        if ',' in preco:
+            preco = preco.replace(',','.')
+            if is_number(preco) is False:
+                print(f'\033[0;31mERR: "{preco}" não aceito!\033[m')
+                preco = str(input(texto))
+            else:
+                break
+    return float(preco)
+
+def is_number(string):
+    '''
+    Verifica se o valor é numérico,
+    usado invés de isnumeric(), porque essa
+    função não funciona com valores float.
+    '''
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
